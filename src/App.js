38e83093e924b3be1c912611c72d045bc532853b
Ben Sayeg <bensayeg@gmail.com>
2021-03-19 09:32:35 -0400

    basic interactive data table Grid component completed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import './Grid.css';
+import Grid from './Grid';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>FizzBizz Scheduler</p>
+      </header>
+      <Grid/>
+    </div>
+  );
+}
+
+export default App;
