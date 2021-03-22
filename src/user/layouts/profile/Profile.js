b0fba46ca2d767abe22ff3aee74c0e8b3e896db5
Ben Sayeg <bensayeg@gmail.com>
2021-03-21 23:32:36 -0400

    minor copy update

--- src/user/layouts/profile/Profile.js
+++ src/user/layouts/profile/Profile.js
@@ -12,7 +12,7 @@ function Profile (props){
       <div className="pure-g">
         <div className="pure-u-1-1">
           <h1>Profile</h1>
-          <p>Change these details in UPort to see them reflected here.</p>
+          <p>Change these details in Metamask to see them reflected here.</p>
           <p>
             <strong>Name</strong><br />
             {authData.name}
