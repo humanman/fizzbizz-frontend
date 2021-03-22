a3abc8bb833bdb0ec12027bfa9e597f28815fe96
Ben Sayeg <bensayeg@gmail.com>
2021-03-21 23:33:22 -0400

    updating reducer bundling

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
-import userReducer from './user/userReducer'
+import userReducer from './reducers/userReducer'
+import statusReducer from './reducers/statusReducer'
 
 const reducer = combineReducers({
   routing: routerReducer,
-  user: userReducer
+  user: userReducer,
+  status: statusReducer
 })
 
 export default reducer
